a62482751d5943a071b0f44249869a09f4f85d2d
xieheng <xieheng0915@gmail.com>
2024-03-28 10:03:57 +0900

    json buildup

--- app.py
+++ app.py
@@ -22,3 +22,22 @@ html_link = "https://solr.apache.org/guide/solr/latest/indexing-guide/indexing-w
 #page = requests.get(html_link)
 #soup = bs(page.text, "html.parser")
 walkthrough_html(html_link)
+
+'''
+json_ = {}
+json_['layer'] = 'h1'
+json_['title'] = 'test'
+unit = {'preamble': 'preamble paragraph'} 
+json_['paragraphs'] = []
+json_['paragraphs'].append(unit)
+unit = {'paragraph': 'test paragraph 2'}
+json_['paragraphs'].append(unit)
+unit = []
+unit_mini = ["table 1", "table 2"]
+unit = {'table paragraph': unit_mini}
+json_['paragraphs'].append(unit)
+unit_mini.append("table 3")
+unit = {'table paragraph': unit_mini}
+print(json.dumps(json_, indent=4))
+'''
+
--- utils.py
+++ utils.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 import json
 
-
 client = OpenAI()
-
 
 def translate_func(text, target_language):
     response = client.completions.create(
@@ -28,9 +26,6 @@ def split_data(docs):
     docs_chunks = text_splitter.split_text(docs)
     return docs_chunks
 
-
-
-
 # find titles by layer
 def getLayerTitles(layer,soup):
     layer_titles = []
@@ -45,62 +40,72 @@ def build_paragraphs(soup_element):
         paragraphs +=p.text
     return paragraphs 
 
-def find_elements(sects, layer):
-    #set json 
-    doc_data = {}
-    #Title
-    layerStr = str('h' + str(layer))
-    print("layer: " + layerStr)
-    doc_data['layer'] = layerStr
+def build_code_blocks(code_blocks):
+    code_data = []
+    for code_block in code_blocks:
+        code_content = code_block.find('code').text
+        code_data.append(code_content)
+    return code_data
+
+def build_paragraph_blocks(paragraphs):
+    paragraph_data = []
+    for paragraph in paragraphs:
+        paragraph_content = paragraph.find('p').text
+        paragraph_data.append(paragraph_content)
+    return paragraph_data
+
+# build json        
+def build_json(sects,layer):
+    doc_data = []
+    #index = 0
     for sect in sects:
-      title = sect.find(layerStr).text
-      print(title)
-      doc_data['title'] = title
-      sectionbody = sect.find_all('div', class_='sectionbody')
-      for section in sectionbody:
-        #Paragraph
-        paragraph = build_paragraphs(section.find('div', class_='paragraph'))
-        print(paragraph)
-        doc_data['paragraph'] = paragraph
-        #Code Block
-        if section.find('div', class_='listingblock'):
-          code_block = section.find('div', class_='listingblock').find('code').text
-          print(code_block)
-        #list
-        if section.find('ul'):
-          ulist = section.find_all('ul')
-          for ul in ulist:
-            litem = ul.find_all('li')
-            for li in litem:
-              print(li.find('p').text)  
-        #table
-        if section.find('table'):
-          pass
-        #attention marks  
-        #find next layer and call regressionly
-        nextSectStr = 'sect' + str(layer)
-        #print("nextSect: " + nextSectStr)
-        if section.find_all('div', class_=nextSectStr):
-          sect_next = section.find_all('div', class_=nextSectStr)
-          find_elements(sect_next, layer +1)
+       sect_data = {}
+       #index += 1
+       #sect_data.update({'id': index})
+       sectname = 'sect' + str(int(layer[-1])-1)
+       sect_data.update({'sect': sectname})
+       # Sect title
+       title = sect.find(layer).text
+       sect_data.update({'title': title})
+       # Sect  paragraph
+       if sect.find_all('div', class_='paragraph'):
+        paragraph_blocks = sect.find_all('div', class_='paragraph')
+        paragraphs = build_paragraph_blocks(paragraph_blocks)
+        sect_data.update({'paragraph': paragraphs})
+       # Sect code block
+       if sect.find_all('div', class_='listingblock'):
+        code_blocks = sect.find_all('div', class_='listingblock')
+        code_data = build_code_blocks(code_blocks)
+        sect_data.update({'code': code_data})
+       # Sect table
+       # Next Sect
+       nextLayer = 'h' + str(int(layer[-1]) + 1)
+       nextSect = 'sect' + str(int(layer[-1]))
+       if sect.find_all('div', class_=nextSect):
+        nextSects = sect.find_all('div', class_=nextSect)
+        nextLayerData = build_json(nextSects, nextLayer)
+        sect_data.update({nextSect: nextLayerData})
+       
+       # Add all the elemet into list 
+       doc_data.append(sect_data)
+ 
     return doc_data
-        
-                        
+
 # scrape html
 def walkthrough_html(html):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     page = requests.get(html, headers)
     soup = bs(page.text, "html.parser")
-    #find titles by layer
-    h2_titles = getLayerTitles("h2",soup)
-    #print(h2_titles)
-    h3_titles = getLayerTitles("h3",soup)
-    #print(h3_titles)
+    # get h1 titles
+    # preamble paragraph
 
-    sect1 = soup.find_all('div', class_='sect1')
-    doc_data = find_elements(sect1, 2)
-    print(doc_data)
+    sects = soup.find_all('div', class_='sect1')
+    json_=build_json(sects, 'h2')
+    print(json.dumps(json_, indent=4))
+
+    #doc_data = find_elements(sect1, 2)
+    #print(json.dumps(doc_data, indent=4))
     
    
             
