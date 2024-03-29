cae15526f35df2c8ab76198afd9f9b5241c62d69
xieheng <xieheng0915@gmail.com>
2024-03-29 10:45:27 +0900

    scrapy after sect2

--- utils.py
+++ utils.py
@@ -54,42 +54,106 @@ def build_paragraph_blocks(paragraphs):
         paragraph_data.append(paragraph_content)
     return paragraph_data
 
-# build json        
-def build_json(sects,layer):
+def get_ulist(ulist):
+    ulist_data = []
+    ul = ulist.find('ul').find_all('li')
+    for li in ul:
+        content = li.find('p').text
+        ulist_data.append(content)
+    
+    return ulist_data
+    
+def get_dlist(dlist):
+    dl_data = []
+    dl_sect = dlist.find('dl')
+    children = dl_sect.findChildren(recursive=False)
+    for child in children:
+        if child.name == 'dt':
+            dl_data.append({'dt': child.text})
+        elif child.name == 'dd':
+            table = child.find('table')
+            if table:
+                td = table.find_all('td')
+                td_data = []
+                for td_ in td:
+                    td_data.append({'td':td_.text})
+                dl_data.append({'table': td_data})
+            paragraph = child.find('div', class_='paragraph')
+            if paragraph:
+                dl_data[-1].update({'paragraph': paragraph.text})
+
+    return dl_data  
+
+# find html elements, build json by sequence
+def build_json_sequence(sects,layer):
+    sect_docs = {}
     doc_data = []
-    #index = 0
     for sect in sects:
-       sect_data = {}
-       #index += 1
-       #sect_data.update({'id': index})
-       sectname = 'sect' + str(int(layer[-1])-1)
-       sect_data.update({'sect': sectname})
-       # Sect title
-       title = sect.find(layer).text
-       sect_data.update({'title': title})
-       # Sect  paragraph
-       if sect.find_all('div', class_='paragraph'):
-        paragraph_blocks = sect.find_all('div', class_='paragraph')
-        paragraphs = build_paragraph_blocks(paragraph_blocks)
-        sect_data.update({'paragraph': paragraphs})
-       # Sect code block
-       if sect.find_all('div', class_='listingblock'):
-        code_blocks = sect.find_all('div', class_='listingblock')
-        code_data = build_code_blocks(code_blocks)
-        sect_data.update({'code': code_data})
-       # Sect table
-       # Next Sect
-       nextLayer = 'h' + str(int(layer[-1]) + 1)
-       nextSect = 'sect' + str(int(layer[-1]))
-       if sect.find_all('div', class_=nextSect):
-        nextSects = sect.find_all('div', class_=nextSect)
-        nextLayerData = build_json(nextSects, nextLayer)
-        sect_data.update({nextSect: nextLayerData})
-       
-       # Add all the elemet into list 
-       doc_data.append(sect_data)
- 
-    return doc_data
+       #call sect process
+        sect_data = build_json_sect1(sect)
+        doc_data.append(sect_data)
+    
+    sect_docs.update({'sect1': doc_data})
+    return sect_docs
+
+# build json for sect1 layer
+def build_json_sect1(sect):
+    sect_data = []
+    # Sect definition
+    nextLayer = 'h3'
+    nextSect = 'sect2'
+    
+    # Sect title
+    title = sect.find('h2').text
+    sect_data.append({'title': title})
+    #first element under sectionbody 
+    sect_body = sect.find('div', class_='sectionbody').find_all('div', recursive=False)
+    for element in sect_body:
+        if 'paragraph' in element['class']:
+            paragraphs = build_paragraphs(element)
+            sect_data.append({'paragraph': paragraphs})
+        elif 'listingblock' in element['class']:
+            code_data = element.find('code').text
+            sect_data.append({'code': code_data})
+        elif 'sect2' in element['class']:
+            sect2_data = build_json_after_sect2(element,'h3')            
+            sect_data.append({'sect2': sect2_data})
+    
+   
+    #return data
+    return sect_data
+
+# build json for sect2 and after
+def build_json_after_sect2(sect, layer):
+    sect_data = []
+    # sect2, h3; sect3, h4; sect4, h5...
+    nextSect = 'sect' + str(int(layer[-1]))
+    nextLayer = 'h' + str(int(layer[-1]) + 1)
+
+    curr_element = sect.find(layer)
+    sect_data.append({'title': curr_element.text})
+    nextSiblings = curr_element.find_next_siblings('div', recursive=False)
+    for element in nextSiblings:
+        if 'paragraph' in element['class']:
+            paragraphs = build_paragraphs(element)
+            sect_data.append({'paragraph': paragraphs})
+        elif 'listingblock' in element['class']:
+            code_data = element.find('code').text
+            sect_data.append({'code': code_data})
+        elif 'dlist' in element['class']:
+            dlist_data = get_dlist(element)
+            sect_data.append({'dlist': dlist_data})
+        elif 'ulist' in element['class']:
+            ulist_data = get_ulist(element)
+            sect_data.append({'ulist': ulist_data})
+        elif nextSect in element['class']:
+            nextSectData = build_json_after_sect2(element, nextLayer)
+            sect_data.append({nextSect: nextSectData})
+        
+
+
+    return sect_data
+
 
 # scrape html
 def walkthrough_html(html):
@@ -101,11 +165,12 @@ def walkthrough_html(html):
     # preamble paragraph
 
     sects = soup.find_all('div', class_='sect1')
-    json_=build_json(sects, 'h2')
+    #json_=build_json(sects, 'h2')
+    json_=build_json_sequence(sects, 'h2')
+    #json_=build_json_for_elements(soup)
     print(json.dumps(json_, indent=4))
 
-    #doc_data = find_elements(sect1, 2)
-    #print(json.dumps(doc_data, indent=4))
+    
     
    
             
